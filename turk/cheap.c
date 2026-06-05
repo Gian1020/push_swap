8f858ca427e94dc3e373242606277be67b2bfafe
Giovanni Pecelli <imgiovannipecelli@protonmail.com>
2026-06-05 16:20:45 +0200

    Update cheap.c

--- turk/cheap.c
+++ turk/cheap.c
@@ -9,31 +9,35 @@ t_stack *find_cheap(t_stack *l_stack)
     {
         if (temp->is_cheap)
             return (temp);
-        temp = temp->next;
-    }
-    return (NULL);
+		temp = temp->next;
+	}
+	return (NULL);
 }
-// da rivedere la comparazione del meno costoso
-void set_cheapest(t_stack *l_stack)
-{
-    t_stack *cheapest_node;
-    t_stack *current;
 
-    if (l_stack == NULL)
-        return ;
-    cheapest_node = l_stack;
-    current = l_stack;
-    while (current != NULL)
-    {
-        current->is_cheap = 0;
-        current = current->next;
-    }
-    current = l_stack; 
-    while (current != NULL)
-    {
-        if (current->total_cost < cheapest_node->total_cost)
-            cheapest_node = current;
-        current = current->next;
-    }
-    cheapest_node->is_cheap = 1;
+void	set_cheapest(t_stack *l_stack)
+{
+	t_stack *cheapest_node;
+	t_stack *current;
+
+	if (l_stack == NULL)
+		return ;
+	current = l_stack;
+	while (current != NULL)
+	{
+		current->is_cheap = 0;
+		current = current->next;
+	}
+	cheapest_node = NULL;
+	current = l_stack; 
+	while (current != NULL)
+	{
+		if (current->total_cost != INT_MAX)
+		{
+			if (current->total_cost < cheapest_node->total_cost)
+				cheapest_node = current;
+		}
+		current = current->next;
+	}
+	if (cheapest_node)
+		cheapest_node->is_cheap = 1;
 }
