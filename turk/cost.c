361f4be5b43c9802ae84f811b127078a00d7dff5
Giovanni Pecelli <imgiovannipecelli@protonmail.com>
2026-06-05 15:53:39 +0200

    Update cost.c

--- turk/cost.c
+++ turk/cost.c
@@ -2,16 +2,22 @@
 
 static void	set_calculate_total_cost(t_stack *node_in_a)
 {
-	if ((node_in_a->cost_a > 0 && node_in_a->cost_b > 0)
-				|| (node_in_a->cost_a < 0 && node_in_a->cost_b < 0))
-		{
-			if (node_in_a->cost_a > node_in_a->cost_b)
-				node_in_a->total_cost = node_in_a->cost_a;
-			else
-				node_in_a->total_cost = node_in_a->cost_b;
-		}
+	if (node_in_a->cost_a > 0 && node_in_a->cost_b > 0)
+	{
+		if (node_in_a->cost_a > node_in_a->cost_b)
+			node_in_a->total_cost = node_in_a->cost_a;
 		else
-        	node_in_a->total_cost = ft_abs(node_in_a->cost_a) + ft_abs(node_in_a->cost_b);
+			node_in_a->total_cost = node_in_a->cost_b;
+	}
+	else if ((node_in_a->cost_a < 0 && node_in_a->cost_b < 0))
+	{
+		if (node_in_a->cost_a < node_in_a->cost_b)
+			node_in_a->total_cost = node_in_a->cost_a;
+		else
+			node_in_a->total_cost = node_in_a->cost_b;
+	}
+	else
+    	node_in_a->total_cost = ft_abs(node_in_a->cost_a) + ft_abs(node_in_a->cost_b);
 }
 
 void    calculate_cost(t_stack *stack_a, t_stack *stack_b)
